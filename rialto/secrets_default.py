c6fa5c6bec947bab92cb5376c2b68ec244521ede
Jeremy Archer <open-source@fatlotus.com>
2012-10-20 11:39:34 -0500

    Add secrets handler.

---
+++ rialto/secrets_default.py
@@ -0,0 +1,9 @@
+# This file is only the default; please change the
+# settings here before uploading to Google App Engine.
+
+SESSION_SECRET = "" # Set this value to a random string.
+  
+  # On a UNIX system, this can be done most easily by
+  # running the following at a command line:
+  # 
+  # dd if=/dev/urandom count=10240 bs=1024 | openssl sha1
